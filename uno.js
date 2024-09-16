716b1602752cb7c28d3ae85fb0b6a59e8349d7c2
MarcelaB21 <111161213+MarcelaB21@users.noreply.github.com>
2024-09-16 18:40:14 -0500

    codigo.js

--- uno.js
+++ uno.js
@@ -1,1 +1,10 @@
 
+ const  respuesta = prompt("te gusto ?:");
+  if(respuesta==="si"){
+       console.log( "Seras mi novia ");
+    }else if (respuesta==="no"){
+        console.log( "Entiendo ,solo amigos ");
+    }else{
+        console.log("piensalo")
+    }
+
